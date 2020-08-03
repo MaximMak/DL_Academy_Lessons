fa95c95bc20c0f7a3ff3aa5904aa359b9da0131a
Maxim <54116778+MaximMak@users.noreply.github.com>
2020-08-03 19:19:15 +0400

    update

--- kurs_1/lesson_9/hw_9.py
+++ kurs_1/lesson_9/hw_9.py
@@ -11,21 +11,6 @@ with tag('html'):
             text('link to url of your')
 
 result = doc.getvalue()
-class Store:
-    """Инициализируем класс магазин, с атрибутами Имя продавца, и категорией продуктов"""
-    def __init__(self, seller, category, selling_plan):
-        self.seller = seller
-        self.category = category
-        self.selling_plan = selling_plan
 
-class Product(Store):
-    def __init__(self, unique_id, product_name, price, amount):
-        self.id = unique_id
-        self.name = product_name
-        self.price = price
-        self.amount = amount
 
-class FruitProduct(Product):
-    def __init__(self, country, shelf_life):
-        self.country = country
-        self.shelf_life = shelf_life
+class
