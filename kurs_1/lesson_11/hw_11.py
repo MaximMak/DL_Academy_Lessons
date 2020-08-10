f042fb5231d35a2358f88d93ace0e11cd51682be
Maxim <54116778+MaximMak@users.noreply.github.com>
2020-08-10 19:07:17 +0400

    9 and 11 remastred

--- kurs_1/lesson_11/hw_11.py
+++ kurs_1/lesson_11/hw_11.py
@@ -86,12 +86,14 @@ class acquire(object):
             lock.release()
         return False
 
+
 # The philosopher thread
 def philosopher(left, right):
     while True:
-        with acquire(left,right):
+        with acquire(left, right):
             time.sleep(0.5)
             print(f'Philosopher at {threading.currentThread()} is eating.')
+
 
 # The chopsticks
 N_FORKS = 5
@@ -106,38 +108,3 @@ phils = [threading.Thread(
 # Run all of the philosophers
 for p in phils:
     p.start()
-
-
-
-
-""" реализация 9_го задания  помощью сторонней бибилиотеки Yattag """
-from yattag import Doc
-
-doc, tag, text = Doc().tagtext()
-
-with tag('html'):
-    with tag('body'):
-        with tag('p', id = 'main'):
-            text('some text in main paragrath!')
-        with tag('a', href='/my-url'):
-            text('link to url of your')
-
-result = doc.getvalue()
-class Store:
-    """Инициализируем класс магазин, с атрибутами Имя продавца, и категорией продуктов"""
-    def __init__(self, seller, category, selling_plan):
-        self.seller = seller
-        self.category = category
-        self.selling_plan = selling_plan
-
-class Product(Store):
-    def __init__(self, unique_id, product_name, price, amount):
-        self.id = unique_id
-        self.name = product_name
-        self.price = price
-        self.amount = amount
-
-class FruitProduct(Product):
-    def __init__(self, country, shelf_life):
-        self.country = country
-        self.shelf_life = shelf_life
