5fda50ecc1f1cb45263c62229ef7c0e17984407e
Maxim <54116778+MaximMak@users.noreply.github.com>
2020-08-10 18:57:48 +0400

    10 reformating

--- kurs_1/lesson_10/hw_10.py
+++ kurs_1/lesson_10/hw_10.py
@@ -6,8 +6,9 @@
 
 """
 
+
 # Класс "Предприятие"
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 class Enterprise:
 
     def __init__(self, name, employees=[]):
@@ -16,6 +17,7 @@ class Enterprise:
 
     def __str__(self):
         return 'Enterprise <Company name: {0}; Employee count: {1}>'.format(self.name, self.employee_count())
+
     # Поиск сотрудника по табельному номеру, возвращает объект типа Сотрудник или False
     def get_emp_by_id(self, emp_id):
         e = False
@@ -27,8 +29,8 @@ class Enterprise:
                 e = self.employees[j]
                 break
             j += 1
-
         return e
+
     # Добавление сотрудника, по одному табельному номеру может быть только один сотрудник
     def add_employee(self, employee):
         if not self.get_emp_by_id(employee.get_emp_id()):
@@ -36,6 +38,7 @@ class Enterprise:
             return True
         else:
             return False
+
     # Удаление сотрудника по табельному номеру
     def rem_employee(self, emp_id):
         e = self.get_emp_by_id(emp_id)
@@ -44,50 +47,61 @@ class Enterprise:
             return True
         else:
             return False
+
     # Количество сотрудников на предприятии
     def employee_count(self):
         return len(self.employees)
+
     # Список сотрудников предприятия
     def employee_list(self):
         for e in self.employees:
             print('  {}'.format(e))
+
     # Выводим информацию о предприятии
     def info(self):
         print(self)
         self.employee_list()
-#------------------------------------------------------------------------------
+
+
+# ------------------------------------------------------------------------------
 # Класс "Человек"
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+
 class Person:
 
     def __init__(self, last_name, first_name, mid_name, birth_dt, phone_num):
-        self.last_name  = last_name
+        self.last_name = last_name
         self.first_name = first_name
-        self.mid_name   = mid_name
-        self.birth_dt   = birth_dt
-        self.phone_num  = phone_num
+        self.mid_name = mid_name
+        self.birth_dt = birth_dt
+        self.phone_num = phone_num
 
     def __str__(self):
         return 'Person <{0} {1} {2}>'.format(self.last_name, self.first_name, self.mid_name)
-#------------------------------------------------------------------------------
+
+
+# ------------------------------------------------------------------------------
 # Класс "Сотрудник"
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+
 class Employee(Person):
 
     def __init__(self, last_name, first_name, mid_name, birth_dt, phone_num, emp_id, depart_id, salary):
         super().__init__(last_name, first_name, mid_name, birth_dt, phone_num)
-        self.emp_id     = emp_id
-        self.depart_id  = depart_id
-        self.salary     = salary
+        self.emp_id = emp_id
+        self.depart_id = depart_id
+        self.salary = salary
 
     def __str__(self):
         return 'Employee <{0}: {1} {2} {3}>'.format(self.emp_id, self.first_name, self.last_name, self.mid_name)
 
     def get_emp_id(self):
         return self.emp_id
-#------------------------------------------------------------------------------
+
+
+# ------------------------------------------------------------------------------
 # Тест
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # Создаём предприятие
 
 print('Создаём предприятие')
