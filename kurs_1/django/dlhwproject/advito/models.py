4a19985348b2371324c2418817d4023fe4179a3d
Maxim <54116778+MaximMak@users.noreply.github.com>
2020-09-09 19:31:21 +0400

    Ok.
    Start of homework for lesson 2.

--- kurs_1/django/dlhwproject/advito/models.py
+++ kurs_1/django/dlhwproject/advito/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Profile(models.Model):
+    '''
+    Модель профиля пользова теля нашего инстаграмма.
+    '''
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='user_profile'
+    )
+    birth_date = models.DateField('Date of birth', null=True, blank=True)
+    about = models.TextField('About', max_length=500, blank=True)
+    avatar = models.ImageField(upload_to='images/', default=None)
+    sale_ad = models.ManyToManyField(User, related_name='ad`s', blank=True)
+
+    def __str__(self):
+        return str(self.user.username)
+
+
+class sale_ad(models.Model):
+    '''
+    Обьявления пользователя.
+    '''
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField(max_length=1000)
+    image = models.ImageField(upload_to='add_images/')
+    date_pud = models.DateTimeField(default=timezone.now)
+    date_edit = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return 'Author {} date {}'.format(self.author.username, self.date_pud)
