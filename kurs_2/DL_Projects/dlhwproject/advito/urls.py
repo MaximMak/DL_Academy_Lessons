790332fd7af508462d1bd412730de91d3e8ea756
Maxim <54116778+MaximMak@users.noreply.github.com>
2020-11-09 00:37:54 +0400

    123

--- kurs_2/DL_Projects/dlhwproject/advito/urls.py
+++ kurs_2/DL_Projects/dlhwproject/advito/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('feed/', views.FeedView.as_view(), name='feed'),
     path('adverts/create/', views.AdvertCreateView.as_view(), name='create_advert'),
     path('adverts/<int:advert_id>/', views.AdvertDetail, name='advert_detail'),
-    path('adverts/<int:advert_id>/edit/', views.EditAdvert.as_view(), name='delete_advert'),
-    path('adverts/<int:advert_id>/delete/', views.AdvertDelete.as_view(), name='delete_advert'),
+    path('adverts/<int:advert_id>/edit/', views.EditAdvert.as_view(), name='edit_advert'),
+    path('adverts/<int:advert_id>/delete/', views.AdvertDelete.as_view(), name='advert-delete'),
     path('adverts/<int:advert_id>/delete_success/', TemplateView.as_view(template_name='advert/delete_success.html'), name='delete-advert-success'),
     path('adverts/<int:advert_id>/like/', views.AdvertLike.as_view, name='advert_like'),
 ]
